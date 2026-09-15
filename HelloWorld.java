f85e10068f3ecb1644bd0046bd81c166b9bc1f8b
Aljoud5 <Jalkhonin@gmail.com>
2026-09-15 23:42:23 +0300

    Update greeting message in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-       System.out.println("Hello World from Joud!");
+       System.out.println("Hello World from Joud and Haya!");
     }
 }
